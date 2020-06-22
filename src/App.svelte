1052ebac34a626a006943b7f93ec62b6c7174f57
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-06-22 07:44:39 +0530

    Add csz for styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,9 @@
 <script>
+        import csz from 'csz';
 	const message = 'Learn Svelte';
 </script>
 
-<style>
+<!-- <style>
 	:global(body) {
 		margin: 0;
 		font-family: Arial, Helvetica, sans-serif;
@@ -37,16 +38,49 @@
 			transform: scale(1.06);
 		}
 	}
-</style>
+</style> -->
 
-<div class="App">
-	<header class="App-header">
-		<img src="/logo.svg" class="App-logo" alt="logo" />
+<div class={csz`
+	:global(body) {
+		margin: 0;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+
+text-align:center;
+`}>
+	<header class={csz`
+                background-color: #F9F6F6;
+		color: #333;
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		font-size: calc(10px + 2vmin);
+        `}>
+		<img src="/logo.svg" class={csz`
+
+                @keyframes App-logo-spin {
+		from {
+			transform: scale(1);
+		}
+		to {
+			transform: scale(1.06);
+		}
+                }
+
+		height: 40vmin;
+		pointer-events: none;
+		margin-bottom: 1.0rem;
+		animation: App-logo-spin infinite 1.6s ease-in-out alternate;
+
+               	
+                `} alt="logo" />
 		<p>
 			Edit <code>src/App.svelte</code> and save to reload.
 		</p>
 		<a
-			class="App-link"
+			class={csz`color:#ff3e00;`}
 			href="https://svelte.dev"
 			target="_blank"
 			rel="noopener noreferrer"
